334f4986c8dcd97399e178f00316c5e7fa8642de
Hagop Awedikian <hagopawedikian@gmail.com>
2025-05-26 00:49:52 +0200

    2022EE

--- Plotter/config/samples_v12.py
+++ Plotter/config/samples_v12.py
@@ -5,15 +5,17 @@ from TauFW.Plotter.sample.utils import LOG, STYLE, ensuredir, repkey, joincuts, 
 from TauFW.Plotter.sample.utils import getsampleset as _getsampleset
 import json
 
-#f = open("../../PicoProducer/samples/nanoaod_sumw_2022_postEE.json")
-#nevts_json = json.load(f)
+f = open("../PicoProducer/samples/nanoaod_sumw_2022_postEE.json") #for 2022postEE
+f_new = open("../PicoProducer/samples/nanoaod_info_2022EE.json") #for 2022EE
+nevts_json_new = json.load(f_new)
+nevts_json = json.load(f)
 
 def getsampleset(channel,era,**kwargs):
   verbosity = LOG.getverbosity(kwargs)
   year     = getyear(era) # get integer year
   fname    = kwargs.get('fname', "$PICODIR/$SAMPLE_$CHANNEL$TAG.root" ) # file name pattern of pico files
   split    = kwargs.get('split',    ['DY'] if 'tau' in channel else [ ] ) # split samples (e.g. DY) into genmatch components
-  join     = kwargs.get('join',     ['VV','Top'] ) # join samples (e.g. VV, top)
+  join     = kwargs.get('join',     ['VV','Top', 'VVV','HTT'] if era=='2022EE' else ['VV','Top'] ) # join samples (e.g. VV, top)
   rmsfs    = ensurelist(kwargs.get('rmsf', [ ])) # remove the tau ID SF, e.g. rmsf=['idweight_2','ltfweight_2']
   addsfs   = ensurelist(kwargs.get('addsf', [ ])) # add extra weight to all samples
   weight   = kwargs.get('weight',   None         ) # weight for all MC samples
@@ -29,7 +31,7 @@ def getsampleset(channel,era,**kwargs):
     join += ['TT','ST']
   
   # SM BACKGROUND MC SAMPLES
-  if '2022_preEE' in era or '2022_postEE' in era or '2023'in era: # so far same samples and cross sections are used for preEE and postEE, if event numbers are set elsewhere then we don't need to add seperate numbers for both eras
+  if '2022_preEE' in era or '2022_postEE' in era or '2022EE' in era or '2023'in era: # so far same samples and cross sections are used for preEE and postEE, if event numbers are set elsewhere then we don't need to add seperate numbers for both eras
     # for now nevts is set to 1 so it isn't taken into account in the scaling of the samples as this will be done elsewhere
     
     kfactor_dy=6282.6/5455.0 # LO->NNLO+NLO_EW k-factor computed for 13.6 TeV [https://twiki.cern.ch/twiki/bin/viewauth/CMS/MATRIXCrossSectionsat13p6TeV]
@@ -139,6 +141,91 @@ def getsampleset(channel,era,**kwargs):
      #   expsamples.append(('DY',"DYto2TautoMuTauh_M-50","Drell-Yan 50 -> tautau -> mu+tauh",5455.0*kfactor_dy,{'extraweight': dyweight})) # LO (using same cross section as inclusive samples), apply correct normalization in stitching
      #   # the cross section for this exact samples is 1885.0 which is ~ 1/3 the total DY->LL cross section (expected since it only selects taus and not electrons and muons)
      #   # the filter efficiency for this sample (due to tau BRs + kinematic cuts on tau decay products) is 2.865e-02 
+    if '2022EE' in era:
+      expsamples = [ # table of MC samples to be converted to Sample objects
+      # GROUP NAME                     TITLE                 XSEC      EXTRA OPTIONS
+      # DY LO samples
+      ('DY', "DYto2L_M_50_madgraphMLM", "Drell-Yan 50", 5455.0 * kfactor_dy, { 'extraweight': dyweight, "nevts":nevts_json_new["DYto2L_M_50_madgraphMLM"]}),
+      ('DY', "DYto2L_M_50_1J_madgraphMLM", "Drell-Yan 1J 50", 978.3 * kfactor_dy, {'extraweight': dyweight, "nevts":nevts_json_new["DYto2L_M_50_1J_madgraphMLM"]}),
+      ('DY', "DYto2L_M_50_2J_madgraphMLM", "Drell-Yan 2J 50", 315.1 * kfactor_dy, {'extraweight': dyweight, "nevts":nevts_json_new["DYto2L_M_50_2J_madgraphMLM"]}),
+      ('DY', "DYto2L_M_50_3J_madgraphMLM", "Drell-Yan 3J 50", 93.7 * kfactor_dy, {'extraweight': dyweight, "nevts":nevts_json_new["DYto2L_M_50_3J_madgraphMLM"]}),
+      ('DY', "DYto2L_M_50_4J_madgraphMLM", "Drell-Yan 4J 50", 45.4 * kfactor_dy, {'extraweight': dyweight, "nevts":nevts_json_new["DYto2L_M_50_4J_madgraphMLM"]}),
+      # DY 10-50 samples
+      # ('DY', "DYto2L_M_10to50_amcatnloFXFX", "Drell-Yan 10-50 NLO", 20950.0, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_10to50_amcatnloFXFX"]}),
+      ('DY', "DYto2L_M_10to50_madgraphMLM", "Drell-Yan 10-50", 17380.0, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_10to50_madgraphMLM"]}),
+      # W + Jets LO samples
+      ('WJ', "WtoLNu_madgraphMLM", "W + jets", 55300.0 * kfactor_wj, {"nevts":nevts_json_new["WtoLNu_madgraphMLM"]}),
+      ('WJ', "WtoLNu_1J_madgraphMLM", "W + 1J", 9128.0 * kfactor_wj, {"nevts":nevts_json_new["WtoLNu_1J_madgraphMLM"]}),
+      ('WJ', "WtoLNu_2J_madgraphMLM", "W + 2J", 2922.0 * kfactor_wj, {"nevts":nevts_json_new["WtoLNu_2J_madgraphMLM"]}),
+      ('WJ', "WtoLNu_3J_madgraphMLM", "W + 3J", 861.3 * kfactor_wj, {"nevts":nevts_json_new["WtoLNu_3J_madgraphMLM"]}),
+      ('WJ', "WtoLNu_4J_madgraphMLM", "W + 4J", 415.4 * kfactor_wj, {"nevts":nevts_json_new["WtoLNu_4J_madgraphMLM"]}),
+      # TTbar
+      ('TT', "TTto2L2Nu", "ttbar 2l2#nu", 80.9 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTto2L2Nu"]}),
+      ('TT', "TTto4Q", "ttbar hadronic", 346.4 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTto4Q"]}),
+      ('TT', "TTtoLNu2Q", "ttbar semileptonic", 334.8 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTtoLNu2Q"]}),
+      #('TT', "TTtoLNu2Q_ext1", "ttbar semileptonic ext1", 334.8 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTtoLNu2Q_ext1"]}),
+      #('TT', "TTto4Q_ext1", "ttbar hadronic ext1", 346.4 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTto4Q_ext1"]}),
+      #('TT', "TTto2L2Nu_ext1", "ttbar 2l2#nu ext1", 80.9 * kfactor_ttbar, {'extraweight': ttweight, "nevts":nevts_json_new["TTto2L2Nu_ext1"]}),
+      # Diboson
+      ('VV', "WW", "WW", 80.23 * kfactor_ww, {"nevts":nevts_json_new["WW"]}),
+      ('VV', "WZ", "WZ", 29.1 * kfactor_wz, {"nevts":nevts_json_new["WZ"]}),
+      ('VV', "ZZ", "ZZ", 12.75 * kfactor_zz, {"nevts":nevts_json_new["ZZ"]}),
+      # Single top NLO samples
+      # ('ST', "ST_t_channel_top_4f_InclusiveDecays", "ST t-channel t", 123.8, {"nevts":nevts_json_new["ST_t_channel_top_4f_InclusiveDecays"]}),
+      # ('ST', "ST_t_channel_antitop_4f_InclusiveDecays", "ST t-channel at", 75.47, {"nevts":nevts_json_new["ST_t_channel_antitop_4f_InclusiveDecays"]}),
+      # ('ST', "ST_tW_top_2L2Nu", "ST tW semileptonic", 3.8, {"nevts":nevts_json_new["ST_tW_top_2L2Nu"]}),
+      # ('ST', "ST_tW_top_2L2Nu_ext1", "ST tW 2l2#nu ext1", 3.8, {"nevts":nevts_json_new["ST_tW_top_2L2Nu_ext1"]}),
+      # ('ST', "ST_tW_antitop_2L2Nu", "ST atW semileptonic", 3.8, {"nevts":nevts_json_new["ST_tW_antitop_2L2Nu"]}),
+      # ('ST', "ST_tW_antitop_2L2Nu_ext1", "ST atW 2l2#nu ext1", 3.8, {"nevts":nevts_json_new["ST_tW_antitop_2L2Nu_ext1"]}),
+      # ('ST', "ST_tW_top_LNu2Q", "ST tW top LNu2Q", 15.8, {"nevts":nevts_json_new["ST_tW_top_LNu2Q"]}),
+      # ('ST', "ST_tW_top_LNu2Q_ext1", "ST tW top LNu2Q ext1", 15.8, {"nevts":nevts_json_new["ST_tW_top_LNu2Q_ext1"]}),
+      # ('ST', "ST_tW_antitop_LNu2Q", "ST atW antitop LNu2Q", 15.9, {"nevts":nevts_json_new["ST_tW_antitop_LNu2Q"]}),
+      # ('ST', "ST_tW_antitop_LNu2Q_ext1", "ST atW antitop LNu2Q ext1", 15.9, {"nevts":nevts_json_new["ST_tW_antitop_LNu2Q_ext1"]}),
+      # Single top LO samples
+      ('ST', "ST_tW_top_4Q", "ST tW top 4Q", 35.99, {"nevts":nevts_json_new["ST_tW_top_4Q"]}),
+      # ('ST', "ST_tW_top_4Q_ext1", "ST tW top 4Q ext1", 35.99, {"nevts":nevts_json_new["ST_tW_top_4Q_ext1"]}),
+      ('ST', "ST_tW_antitop_4Q", "ST tW antitop 4Q", 36.05, {"nevts":nevts_json_new["ST_tW_antitop_4Q"]}),
+      # ('ST', "ST_tW_antitop_4Q_ext1", "ST tW antitop 4Q ext1", 36.05, {"nevts":nevts_json_new["ST_tW_antitop_4Q_ext1"]}),
+      # Triboson 
+      ('VVV', "WWW_4F", "WWW 4F", 0.2328, {"nevts":nevts_json_new["WWW_4F"]}),
+      ('VVV', "WWZ_4F", "WWZ 4F", 0.1851, {"nevts":nevts_json_new["WWZ_4F"]}),
+      ('VVV', "WZZ", "WZZ", 0.06206, {"nevts":nevts_json_new["WZZ"]}),
+      ('VVV', "ZZZ", "ZZZ", 0.01591, {"nevts":nevts_json_new["ZZZ"]}),
+      # CP_Signal H->tau tau
+      # ('HTT','GluGluHTo2Tau_UncorrelatedDecay_CPodd_UnFiltered_ProdAndDecay', "ggH CPodd UnFiltered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_CPodd_UnFiltered_ProdAndDecay"]}),
+      # ('HTT','GluGluHTo2Tau_UncorrelatedDecay_MM_UnFiltered_ProdAndDecay', "ggH MM UnFiltered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_MM_UnFiltered_ProdAndDecay"]}),
+      # ('HTT','GluGluHTo2Tau_UncorrelatedDecay_SM_UnFiltered_ProdAndDecay', "ggH SM UnFiltered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_SM_UnFiltered_ProdAndDecay"]}),
+      # ('HTT','ZHToTauTau_UncorrelatedDecay_UnFiltered', "ZH UnFiltered", 0.05920, {"nevts":nevts_json_new["ZHToTauTau_UncorrelatedDecay_UnFiltered"]}),
+      # ('HTT','GluGluHTo2Tau_UncorrelatedDecay_UnFiltered', "ggH UnFiltered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_UnFiltered"]}),
+      # ('HTT','WplusHToTauTau_UncorrelatedDecay_UnFiltered', "WplusH UnFiltered", 0.05575, {"nevts":nevts_json_new["WplusHToTauTau_UncorrelatedDecay_UnFiltered"]}),
+      # ('HTT','WminusHToTauTau_UncorrelatedDecay_UnFiltered', "WminusH UnFiltered", 0.03561, {"nevts":nevts_json_new["WminusHToTauTau_UncorrelatedDecay_UnFiltered"]}),
+      # ('HTT','VBFHToTauTau_UncorrelatedDecay_UnFiltered', "VBFH UnFiltered", 0.2558, {"nevts":nevts_json_new["VBFHToTauTau_UncorrelatedDecay_UnFiltered"]}),
+      ('HTT','GluGluHTo2Tau_UncorrelatedDecay_CPodd_Filtered_ProdAndDecay', "ggH CPodd Filtered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_CPodd_Filtered_ProdAndDecay"]}),
+      ('HTT','GluGluHTo2Tau_UncorrelatedDecay_MM_Filtered_ProdAndDecay', "ggH MM Filtered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_MM_Filtered_ProdAndDecay"]}),
+      ('HTT','GluGluHTo2Tau_UncorrelatedDecay_SM_Filtered_ProdAndDecay', "ggH SM Filtered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_SM_Filtered_ProdAndDecay"]}),
+      ('HTT','ZHToTauTau_UncorrelatedDecay_Filtered', "ZH Filtered", 0.05920, {"nevts":nevts_json_new["ZHToTauTau_UncorrelatedDecay_Filtered"]}),
+      ('HTT','GluGluHTo2Tau_UncorrelatedDecay_Filtered', "ggH Filtered", 3.2759, {"nevts":nevts_json_new["GluGluHTo2Tau_UncorrelatedDecay_Filtered"]}),
+      ('HTT','WplusHToTauTau_UncorrelatedDecay_Filtered', "WplusH Filtered", 0.05575, {"nevts":nevts_json_new["WplusHToTauTau_UncorrelatedDecay_Filtered"]}),
+      ('HTT','WminusHToTauTau_UncorrelatedDecay_Filtered', "WminusH Filtered", 0.03561, {"nevts":nevts_json_new["WminusHToTauTau_UncorrelatedDecay_Filtered"]}),
+      ('HTT','VBFHToTauTau_UncorrelatedDecay_Filtered', "VBFH Filtered", 0.2558, {"nevts":nevts_json_new["VBFHToTauTau_UncorrelatedDecay_Filtered"]}),
+      # DY NLO samples
+      # ('DY', "DYto2L_M_50_amcatnloFXFX", "Drell-Yan 50 NLO", 6748.0 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_amcatnloFXFX_ext1", "Drell-Yan 50 NLO ext1", 6748.0 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_amcatnloFXFX_ext1"]}),
+      # ('DY', "DYto2L_M_50_0J_amcatnloFXFX", "Drell-Yan 0J 50 NLO", 5364.0 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_0J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_1J_amcatnloFXFX", "Drell-Yan 1J 50 NLO", 1019.0 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_2J_amcatnloFXFX", "Drell-Yan 2J 50 NLO", 375.3 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_2J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_40to100_1J_amcatnloFXFX", "Drell-Yan 50 1J 40-100 NLO", 475.3 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_40to100_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_100to200_1J_amcatnloFXFX", "Drell-Yan 50 1J 100-200 NLO", 45.42 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_100to200_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_200to400_1J_amcatnloFXFX", "Drell-Yan 50 1J 200-400 NLO", 3.382 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_200to400_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_400to600_1J_amcatnloFXFX", "Drell-Yan 50 1J 400-600 NLO", 0.1162 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_400to600_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_600_1J_amcatnloFXFX", "Drell-Yan 50 1J 600+ NLO", 0.01392 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_600_1J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_40to100_2J_amcatnloFXFX", "Drell-Yan 50 2J 40-100 NLO", 179.3 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_40to100_2J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_100to200_2J_amcatnloFXFX", "Drell-Yan 50 2J 100-200 NLO", 51.68 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_100to200_2J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_200to400_2J_amcatnloFXFX", "Drell-Yan 50 2J 200-400 NLO", 7.159 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_200to400_2J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_400to600_2J_amcatnloFXFX", "Drell-Yan 50 2J 400-600 NLO", 0.4157 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_400to600_2J_amcatnloFXFX"]}),
+      # ('DY', "DYto2L_M_50_PTLL_600_2J_amcatnloFXFX", "Drell-Yan 50 2J 600+ NLO", 0.07019 * kfactor_dy_NLO, {'extraweight': dyweight, "nevts": nevts_json_new["DYto2L_M_50_PTLL_600_2J_amcatnloFXFX"]}),
+    ]   
+
   else:
     LOG.throw(IOError,"Did not recognize era %r!"%(era))
   
@@ -150,6 +237,7 @@ def getsampleset(channel,era,**kwargs):
       print("dataset = ", dataset) 
       #dataset = "SingleMuon_Run%d?"%year # need this one as well for C
       # TODO: need to somehow handle that we need SingleMuonC, MuonC, and MuonD for preEE
+    elif era=='2022EE': dataset = "Muon_Run%d*" % year  
     elif era=='2022_postEE': dataset = "Muon_Run%d?"%year
     elif '2023' in era: dataset = "Muon*"
     else: dataset = "SingleMuon_Run%d?"%year
@@ -176,7 +264,7 @@ def getsampleset(channel,era,**kwargs):
     weight = ""
   #elif channel in ['mutau','etau']:
   if 'mutau' in channel or 'etau' in channel:
-    weight = "genweight*trigweight*puweight*idisoweight_1*idweight_2*ltfweight_2"
+    weight = "sign(genweight)*trigweight*puweight*idisoweight_1*idweight_2*ltfweight_2*idweight_dm_2" # "*sign(genweight)" 
   elif channel in ['tautau','ditau']:
     weight = "genweight*trigweight*puweight*idweight_1*idweight_2*ltfweight_1*ltfweight_2"
   else: # mumu, emu, ...
@@ -199,10 +287,18 @@ def getsampleset(channel,era,**kwargs):
       sampleset.stitch("W*LNu*Jets*",    incl='WtoLNu-4Jets',  name='WJ', cme=cme) # W + jets
   elif '2022_preEE' in era:
       sampleset.stitch("W*LNu*Jets*",    incl='WJetsToLNu-4Jets',  name='WJ', cme=cme) # W + jets
-  sampleset.stitch("DYto2L-4Jets_MLL-50*", incl='DYto2L-4Jets_MLL-50', name="DY_M50", cme=cme) 
-
+  if '2022EE' in era:
+    sampleset.stitch("W*LNu*",    incl='WtoLNu_madgraphMLM',  name='WJ', cme=cme) # W + jets
+    sampleset.stitch("DYto2L*", incl='DYto2L_M_50_madgraphMLM', name="DY", cme=cme)
+    #sampleset.stitch("DYto2L*amcatnloFXFX*", incl='DYto2L_M_50_amcatnloFXFX_ext1', name="DY", cme=cme) # Drell-Yan NLO
+  else:
+      sampleset.stitch("DYto2L-4Jets_MLL-50*", incl='DYto2L-4Jets_MLL-50', name="DY_M50", cme=cme)  
   # JOIN
   sampleset.join('DY', name='DY' ) # Drell-Yan, M < 50 GeV + M > 50 GeV
+  if '2022EE' in era:
+    sampleset.join('VVV', 'WWW_4F', 'WWZ_4F', 'WZZ' , 'ZZZ',name='Triboson' ) # VVV
+    if 'HTT' in join:
+      sampleset.join('HTT', '*UncorrelatedDecay*', name='Higgs')
   if 'VV' in join:
     sampleset.join('VV','WZ','WW','ZZ', name='VV' ) # Diboson
   if 'TT' in join and era!='year':
@@ -211,6 +307,7 @@ def getsampleset(channel,era,**kwargs):
     sampleset.join('ST', name='ST' ) # single top
   if 'Top' in join:
     sampleset.join('TT','ST', name='Top' ) # ttbar + single top
+  sampleset.printtable(merged=True, split=True)
   
   # SPLIT
   # Note: titles are set via STYLE.sample_titles
