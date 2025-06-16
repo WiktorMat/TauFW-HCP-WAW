0c16dc6ac52efefb58fe3f927c58189c20e7887c
Hagop Awedikian <hagopawedikian@gmail.com>
2025-06-16 17:05:37 +0200

    to be fixed further (add common MTT in ModuleTauPair)

--- PicoProducer/python/analysis/TreeProducerTauTau.py
+++ PicoProducer/python/analysis/TreeProducerTauTau.py
@@ -104,6 +104,8 @@ class TreeProducerTauTau(TreeProducerTauPair):
       self.addBranch('idweight_tight_2',         'f', 1., title="tau ID efficiency SF")
       self.addBranch('ltfweight_1',              'f', 1., title="lepton -> tau fake rate SF")
       self.addBranch('ltfweight_2',              'f', 1., title="lepton -> tau fake rate SF")
+      self.addBranch('tau_pt_preSF',      'f', title="no correction applied to tau pt")
+      self.addBranch('tes_sf',      'f', title="tau energy scale correction")
       if module.dosys: # systematic variation (only for nominal tree)
         self.addBranch('idweightUp_1',           'f', 1.)
         self.addBranch('idweightUp_2',           'f', 1.)
